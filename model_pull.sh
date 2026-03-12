bef21ea6544cae4c64469db15ff0e986fb7e7121
Skye Lewallen <lewallen4@gmail.com>
2026-03-12 08:57:48 -0700

    Update model_pull.sh

--- model_pull.sh
+++ model_pull.sh
@@ -2,3 +2,5 @@
 echo " "
 echo "Pulling Model"
 echo " "
+
+wget -q --show-progress -O model.gguf "https://huggingface.co/TheBloke/Mistral-7B-Instruct-v0.2-GGUF/resolve/main/mistral-7b-instruct-v0.2.Q4_0.gguf"
