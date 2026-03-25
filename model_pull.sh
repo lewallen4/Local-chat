1dbeaacba723367145c45e84d59a816dfc537e43
Skye Lewallen <lewallen4@gmail.com>
2026-03-25 13:57:19 -0700

    Update model_pull.sh

--- model_pull.sh
+++ model_pull.sh
@@ -10,3 +10,6 @@ curl -L --progress-bar -o model.gguf "https://huggingface.co/TheBloke/Mistral-7B
 
 # no wget on our network so pivoted
 # wget -q --show-progress -O model.gguf "https://huggingface.co/TheBloke/Mistral-7B-Instruct-v0.2-GGUF/resolve/main/mistral-7b-instruct-v0.2.Q4_0.gguf"
+
+
+# use the meta model llama4 scout, with 128 gigs of ram would be idea. 64 if we have to MIGHT work
