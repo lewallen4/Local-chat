47df8208c98291d3ed922b966a00478c77b5800e
Skye Lewallen <lewallen4@gmail.com>
2026-03-31 21:23:09 -0700

    Update model_pull.sh

--- model_pull.sh
+++ model_pull.sh
@@ -6,11 +6,24 @@ echo " "
 
 cd server/models
 
+
+### Here's the models, theres a selecter on the mod version, this one is just to be normal
+
+
+# use this for the good one 35b
 #curl -L --progress-bar -o model.gguf "https://huggingface.co/ibm-granite/granite-4.0-h-small-GGUF/resolve/main/granite-4.0-h-small-Q4_K_M.gguf"
 
+# use this for the testing as bad one 1b
 curl -L --progress-bar -o model.gguf "https://huggingface.co/Qwen/Qwen2.5-0.5B-Instruct-GGUF/resolve/main/qwen2.5-0.5b-instruct-q4_k_m.gguf"
 
-# no wget on our network so pivoted
+# The French One 7b
+#curl -L --progress-bar -o model.gguf "https://huggingface.co/TheBloke/Mistral-7B-Instruct-v0.2-GGUF/resolve/main/mistral-7b-instruct-v0.2.Q4_0.gguf"
+
+
+
+
+
+# storing if you dont have curl and need wget
 # wget -q --show-progress -O model.gguf "https://huggingface.co/TheBloke/Mistral-7B-Instruct-v0.2-GGUF/resolve/main/mistral-7b-instruct-v0.2.Q4_0.gguf"
 
 
