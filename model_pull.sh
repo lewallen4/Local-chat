dfb2eded72bfe08ba2316b438000c5133f13cb8d
Skye Lewallen <lewallen4@gmail.com>
2026-03-31 21:19:05 -0700

    Change model download URL in model_pull.sh

--- model_pull.sh
+++ model_pull.sh
@@ -6,7 +6,7 @@ echo " "
 
 cd server/models
 
-curl -L --progress-bar -o model.gguf "https://huggingface.co/TheBloke/Mistral-7B-Instruct-v0.2-GGUF/resolve/main/mistral-7b-instruct-v0.2.Q4_0.gguf"
+curl -L --progress-bar -o model.gguf "https://huggingface.co/ibm-granite/granite-4.0-h-small-GGUF/resolve/main/granite-4.0-h-small-Q4_K_M.gguf"
 
 # no wget on our network so pivoted
 # wget -q --show-progress -O model.gguf "https://huggingface.co/TheBloke/Mistral-7B-Instruct-v0.2-GGUF/resolve/main/mistral-7b-instruct-v0.2.Q4_0.gguf"
