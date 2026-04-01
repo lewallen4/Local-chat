1d5502c0c09fdfd6583b09e647648d5696c37dff
Skye Lewallen <lewallen4@gmail.com>
2026-03-31 21:20:55 -0700

    Update model download URL in model_pull.sh
    Replaced the model download URL in model_pull.sh.

--- model_pull.sh
+++ model_pull.sh
@@ -6,7 +6,9 @@ echo " "
 
 cd server/models
 
-curl -L --progress-bar -o model.gguf "https://huggingface.co/ibm-granite/granite-4.0-h-small-GGUF/resolve/main/granite-4.0-h-small-Q4_K_M.gguf"
+#curl -L --progress-bar -o model.gguf "https://huggingface.co/ibm-granite/granite-4.0-h-small-GGUF/resolve/main/granite-4.0-h-small-Q4_K_M.gguf"
+
+curl -L --progress-bar -o model.gguf "https://huggingface.co/Qwen/Qwen2.5-0.5B-Instruct-GGUF/resolve/main/qwen2.5-0.5b-instruct-q4_k_m.gguf"
 
 # no wget on our network so pivoted
 # wget -q --show-progress -O model.gguf "https://huggingface.co/TheBloke/Mistral-7B-Instruct-v0.2-GGUF/resolve/main/mistral-7b-instruct-v0.2.Q4_0.gguf"
