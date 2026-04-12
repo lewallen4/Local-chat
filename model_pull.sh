2790f5ac5d60936546b3bd6e963fb5f248aad8f8
Skye Lewallen <lewallen4@gmail.com>
2026-04-11 23:10:52 -0700

    Update model_pull.sh

--- model_pull.sh
+++ model_pull.sh
@@ -15,12 +15,13 @@ cd server/models
 
 # use this for the testing as bad one 1b
 # curl -L --progress-bar -o model.gguf "https://huggingface.co/Qwen/Qwen2.5-0.5B-Instruct-GGUF/resolve/main/qwen2.5-0.5b-instruct-q4_k_m.gguf"
+curl -L --progress-bar -o model.gguf "https://huggingface.co/unsloth/Qwen3.5-0.8B-GGUF/resolve/main/Qwen3.5-0.8B-BF16.gguf"
 
 # The French One 7b
 # curl -L --progress-bar -o model.gguf "https://huggingface.co/TheBloke/Mistral-7B-Instruct-v0.2-GGUF/resolve/main/mistral-7b-instruct-v0.2.Q4_0.gguf"
 
 # ibm testing model
-curl -L -o model.gguf "https://huggingface.co/ibm-granite/granite-4.0-h-1b-GGUF/resolve/main/granite-4.0-h-1b-Q4_K_M.gguf"
+# curl -L -o model.gguf "https://huggingface.co/ibm-granite/granite-4.0-h-1b-GGUF/resolve/main/granite-4.0-h-1b-Q4_K_M.gguf"
 
 
 
